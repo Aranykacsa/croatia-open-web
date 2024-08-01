a893cd79cd1aeb5658a3f6b8720cd4fb1729cf84
Aranykacsa <zalanvadasz@gmail.com>
2024-08-01 14:57:34 +0200

    mod: main page, packages

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,25 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    import Header from "$lib/components/Header.svelte";
+    import News from "$lib/components/News.svelte";
+    import Footer from "$lib/components/Footer.svelte";
+
+    type CardData = {
+        title: string;
+        description: string;
+        imgUrl?: string;
+    };
+
+    const cards: CardData[] = [
+        {title: 'Card 1', description: 'This is the first card', imgUrl:'./favicon.png'},
+        {title: 'Card 2', description: 'This is the second card'},
+        {title: 'Card 3', description: 'This is the third card'},
+    ];
+
+</script>
+
+<Header/>
+{#each cards as card}
+    <News news={card}/>
+{/each}
+
+<Footer url={"https://www.facebook.com/croatiaopen.orienteering"}/>
